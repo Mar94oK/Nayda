2b9e64c25ffe19fe918956e7aa43336beb835d94
Maksim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2018-01-25 14:06:07 +0300

    HandCover Set for HandSecondaryPlayer.

--- Nayda/handsecondaryplayer.cpp
+++ Nayda/handsecondaryplayer.cpp
@@ -6,6 +6,22 @@ HandSecondaryPlayer::HandSecondaryPlayer(QWidget *parent) :
     ui(new Ui::HandSecondaryPlayer)
 {
     ui->setupUi(this);
+
+
+    QRect HW_Screen_Size = QApplication::desktop()->screenGeometry();
+    int HW_Screen_Size_Width = HW_Screen_Size.width();
+    int HW_Screen_Size_Height = HW_Screen_Size.height();
+
+    QPixmap pxmpHandSecondaryPlayer("Pictures/handCover/HandCover.png");
+    QPalette plte_handSecondaryPlayer;
+    qDebug () << "Size: " << size();
+    plte_handSecondaryPlayer.setBrush(QPalette::Background, QBrush(pxmpHandSecondaryPlayer.scaled(_race_class_btn_size_width*HW_Screen_Size_Width,
+                                                                                  _race_class_btn_size_height*HW_Screen_Size_Height,
+                                                                                  Qt::IgnoreAspectRatio, Qt::SmoothTransformation)));
+    setPalette(plte_handSecondaryPlayer);
+    setAutoFillBackground(true);
+
+
 }
 
 HandSecondaryPlayer::~HandSecondaryPlayer()
--- Nayda/handsecondaryplayer.h
+++ Nayda/handsecondaryplayer.h
@@ -2,6 +2,9 @@
 #define HANDSECONDARYPLAYER_H
 
 #include <QWidget>
+#include <QDesktopWidget>
+#include <QRect>
+#include <QDebug>
 
 namespace Ui {
 class HandSecondaryPlayer;
@@ -17,6 +20,14 @@ public:
 
 private:
     Ui::HandSecondaryPlayer *ui;
+
+private:
+
+    //setup the koefficients;
+    const float _race_class_btn_size_geometric_width_to_height_ratio = 2.71f;
+    const float _race_class_btn_size_width = 0.035f;
+    const float _race_class_btn_size_height = _race_class_btn_size_geometric_width_to_height_ratio*_race_class_btn_size_width;
+
 };
 
 #endif // HANDSECONDARYPLAYER_H
