fd423d17daabcce0b98725a4fefdcf40904aef2e
Maxim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2018-06-18 18:13:40 +0300

    GUI Style refactoring for ServerSettings

--- Nayda/helpserversettings.cpp
+++ Nayda/helpserversettings.cpp
@@ -1,11 +1,14 @@
 #include "helpserversettings.h"
 #include "ui_helpserversettings.h"
+#include <QFile>
+#include <QTextStream>
 
 HelpServerSettings::HelpServerSettings(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::HelpServerSettings)
 {
     ui->setupUi(this);
+    setUpSettingsFilePath("qrc:/HelpDocs/HelpServerSettings.rtf");
     setUpSettingsTextBrowser();
 }
 
@@ -14,10 +17,19 @@ HelpServerSettings::~HelpServerSettings()
     delete ui;
 }
 
+
+
 void HelpServerSettings::setUpSettingsTextBrowser()
 {
     ui->textBrowser->openExternalLinks();
     ui->textBrowser->acceptRichText();
     ui->textBrowser->openExternalLinks();
     ui->textBrowser->setReadOnly(true);
+
+    QFile file(_filePath);
+    file.open(QIODevice::ReadOnly);
+    QTextStream stream(&file);
+    QString content = stream.readAll();
+    file.close();
+    ui->textBrowser->setPlainText(content);
 }
--- Nayda/helpserversettings.h
+++ Nayda/helpserversettings.h
@@ -11,13 +11,24 @@ class HelpServerSettings : public QDialog
 {
     Q_OBJECT
 
+private:
+    
+    QString _filePath;
+
 public:
     explicit HelpServerSettings(QWidget *parent = 0);
     ~HelpServerSettings();
+    
+    void setUpSettingsFilePath(QString path)
+    {
+        _filePath = path;
+    }
 
 private:
     Ui::HelpServerSettings *ui;
     void setUpSettingsTextBrowser();
+    
+    
 };
 
 #endif // HELPSERVERSETTINGS_H
