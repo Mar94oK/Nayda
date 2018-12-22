5e66d8e6ccf2468cc1c1a8377c4bcfe7e99b35a0
M.I.Uvarenkov <h.uvarenkov@gmail.com>
2018-12-22 20:34:46 +0300

    Refactroing hand.ccp removing odd unused effects.

--- Nayda/hand.cpp
+++ Nayda/hand.cpp
@@ -8,49 +8,16 @@ Hand::Hand(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QRect HW_Screen_Size = QApplication::desktop()->screenGeometry();
-    int HW_Screen_Size_Width = HW_Screen_Size.width();
-    int HW_Screen_Size_Height = HW_Screen_Size.height();
-
-    //setup the koefficients;
-    const float handCard_btn_size_geometric_width_to_height_ratio = 2.71f;
-    const float handCard_btn_size_width = 0.0415f;
-    const float handCard_btn_size_height = handCard_btn_size_geometric_width_to_height_ratio*handCard_btn_size_width;
-
-    //set the maximum Height of the "Hand" Widget
-    setMaximumHeight(HW_Screen_Size_Height*handCard_btn_size_height);
-
-    //place picture
-#ifndef USE_RESOURCES
-    QPixmap pxmp_HandCover("Pictures/Hand/sunRiseMiddle.png");
-#else
-    QPixmap pxmp_HandCover(":/Pictures/Hand/sunRiseMiddle.png");
-#endif
-
-    QPalette plte_HandCover;
-    plte_HandCover.setBrush(backgroundRole(),
-    QBrush(pxmp_HandCover.scaled(handCard_btn_size_width*HW_Screen_Size_Width,
-                                                             handCard_btn_size_height*HW_Screen_Size_Height,
-                                                             Qt::IgnoreAspectRatio, Qt::SmoothTransformation)));
-
-    //do not fill the background to make it transparent
-    setAutoFillBackground(false);
-    //setPalette(plte_HandCover);
-
     _showCardsTimer = new QTimer(this);
     _showCardsTimer->setSingleShot(true);
-    //connect timeout issue
     connect(_showCardsTimer, &QTimer::timeout, this, &Hand::SlotShowTheCardInCentreSlot);
-//    connect(_showCardsTimer, &QTimer::timeout ,this, &Hand::_showTheCardNearItsPositionSlot);
     connect(this, &Hand::SignalCardIsPreparedToBePlayed, this, &Hand::SlotCardIsPreparedToBePlayedFromHand);
-//    qDebug() << "The Size Of Cards On Hands, Height: " << size().height();
 
 #ifdef __linux__
     _debounceTimer = new QTimer(this);
     _debounceTimer->setSingleShot(true);
     connect(_debounceTimer, &QTimer::timeout, this, &Hand::SlotDebounceTimerHandler);
 #endif
-
 
 }
 
