7b92f2d847d8cf9bb3bc4717c1609811ad8728d4
Max <h.uvarenkov@gmail.com>
2019-02-23 21:26:54 +0300

    Continuing to work with log Levels

--- Nayda/smartqtlogger.h
+++ Nayda/smartqtlogger.h
@@ -2,22 +2,54 @@
 #define SMARTQTLOGGER_H
 #include <QDebug>
 
+
+
 enum class LoggerLevel
 {
-    Info,
+    Info = 0,
     Warning,
     Debug,
     Error,
     Essential,
     TaskCompletion,
-    Observation
+    Observation,
+    All
 };
 
-template <typename T>
+static const LoggerLevel logLevel = LoggerLevel::All;
+
 class Logger
 {
+private:
+
+    static const std::vector<LoggerLevel> levels;
+    LoggerLevel _currentMessageLevel = LoggerLevel::All;
+
+private:
+
+    QString GetLogLevelName(LoggerLevel type)
+    {
+        switch (type)
+        {
+        case LoggerLevel::Info:
+            return "===Info===";
+
+        default:
+            break;
+        }
+    }
+
+public:
+
+    template <typename T>
+    Logger& operator<< (const T& message)
+    {
+        if (std::find(levels.begin(), levels.end(), _currentMessageLevel) != levels.end() )
+
+    }
 
 };
 
+const std::vector<LoggerLevel>Logger::levels = { LoggerLevel::All };
 
 #endif // SMARTQTLOGGER_H
